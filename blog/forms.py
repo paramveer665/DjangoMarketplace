0da7d180a29b05587fcaf274192699060c5e4c02
paramveer665 <paramveerchauhan665@gmail.com>
2025-09-25 23:14:34 +0530

    fixed Ui msitakes in after last commit

--- blog/forms.py
+++ blog/forms.py
@@ -5,5 +5,20 @@ from .models import Blog
 
 class createBlogForm(forms.ModelForm):
     class Meta:
-        model=Blog
-        fields=['blogtitle','content']
+        model = Blog
+        fields = ['blogtitle', 'content']
+        widgets = {
+            'blogtitle': forms.TextInput(
+                attrs={
+                    'class': 'form-control',
+                    'placeholder': 'Enter the title of your blog'
+                }
+            ),
+            'content': forms.Textarea(
+                attrs={
+                    'class': 'form-control',
+                    'placeholder': 'Write your blog content here',
+                    'rows': 5
+                }
+            ),
+        }
--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from django.urls import path,include
 from .views import blog_view,create_blog_view,my_blogs_view
 
 urlpatterns = [
-    path('',blog_view),
+    path('',blog_view,name="blogs"),
     path('create/',create_blog_view,name="createblog"),
     path('myblogs/',my_blogs_view,name='myblogs')
     
